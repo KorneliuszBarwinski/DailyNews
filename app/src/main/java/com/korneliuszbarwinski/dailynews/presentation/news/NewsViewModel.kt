379058b2acc9a3def697a7200227f3e0c3ef7e82
KorneliuszBarwinski <33134673+KorneliuszBarwinski@users.noreply.github.com>
2022-11-14 22:57:48 +0100

    Added SwipeToRefresh (#4)
    * - added DI using HILT
    - added domain models, repository and usecase
    - added data dtos, mapper, repository implementation and api
    - in the ui added navigation component, NewsFragment with RecyclerView and Adapter, and basic ViewModel
    - added ViewUtils and show/hide functions
    - added Glide to load article image
    
    * - added SwipeToRefresh functionality to NewsFragment

--- app/src/main/java/com/korneliuszbarwinski/dailynews/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/korneliuszbarwinski/dailynews/presentation/news/NewsViewModel.kt
@@ -8,23 +8,38 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import com.korneliuszbarwinski.dailynews.common.Resource
 import com.korneliuszbarwinski.dailynews.domain.model.Article
 import com.korneliuszbarwinski.dailynews.domain.usecase.GetNewsUseCase
+import com.korneliuszbarwinski.dailynews.domain.usecase.RefreshNewsUseCase
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 import javax.inject.Inject
 
 @HiltViewModel
-class NewsViewModel   @Inject constructor (private val getNewsUseCase: GetNewsUseCase) : ViewModel() {
+class NewsViewModel
+@Inject constructor(
+    private val getNewsUseCase: GetNewsUseCase,
+    private val refreshNewsUseCase: RefreshNewsUseCase
+) : ViewModel() {
 
     private val _latestNews = MutableLiveData<Resource<List<Article>>>()
     val latestNews: LiveData<Resource<List<Article>>>
         get() = _latestNews
 
-    init{
+    init {
         getLatestNews()
     }
-    private fun getLatestNews(){
+
+    private fun getLatestNews() {
         _latestNews.value = Resource.Loading()
-        viewModelScope.launch{
+        viewModelScope.launch {
             _latestNews.value = getNewsUseCase.invoke()
+        }
+    }
+
+    fun refreshNews() {
+        _latestNews.value = Resource.Loading()
+        viewModelScope.launch {
+            _latestNews.value = refreshNewsUseCase.invoke()
         }
     }
 }
