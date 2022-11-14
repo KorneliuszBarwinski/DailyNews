cf7b15c2b168df01cd24494c3dab2c04a5ca9e26
Korneliusz Barwinski <korneliusz.barwinski@gmail.com>
2022-11-14 20:05:52 +0100

    - removed api key

--- app/src/main/java/com/korneliuszbarwinski/dailynews/common/Constants.kt
+++ app/src/main/java/com/korneliuszbarwinski/dailynews/common/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
     const val DEFAULT_COUNTRY_CODE = "pl"
 
-    const val API_KEY = "40df7b1794b74c9bb797be59f56cbc12"
+    const val API_KEY = "YOUR_API_KEY_HERE" //I left place for API_KEY here as simplification to make it easier to build the application by someone else - you should not push API keys on repos
 }
